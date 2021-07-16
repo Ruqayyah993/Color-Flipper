9dcdbaa0dbb378fdd1b0ed5ef9438210d4962365
Ruqayyah <86515074+Ruqayyah993@users.noreply.github.com>
2021-07-16 17:52:49 +0100

    Color Flipper

---
+++ app.js
@@ -0,0 +1,16 @@
+const colors = [ "purple", "blue","brown", "yellow","chocolate" "aliceblue", "antiquewhite","aqua", 
+ "chocolate", "crimson", "coral", "cyan","cadetblue"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function () {
+  const randomNumber = getRandomNumber();
+  // console.log(randomNumber);
+
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length);
+}
